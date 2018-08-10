0bf24a3134faa68cf19a6d34bcd6d6bc0d0ee5b9
mohithpothineni <mohith.pothineni@msitprogram.net>
2018-08-10 14:55:47 +0530

    mcheck wrkng1

---
+++ mcheck.py
@@ -0,0 +1,46 @@
+''' eval like 
+    no git push
+'''
+import os
+import subprocess
+import sys
+
+try:
+    lis_dir = os.listdir("./testcases")
+    #print(lis_dir)
+except Exception as e:
+    print(e)
+else:
+    n=len(lis_dir)
+    n -= 1
+    n2 = n//2
+    sum = 0
+    for i in range(n2):
+        f = open('./testcases/'+lis_dir[i],'rb')
+        inp_ = f.read()
+        f.close()
+
+        p=subprocess.run([sys.argv[1]],shell=True,check=True,input=inp_,stdout=subprocess.PIPE)
+        f = open('./testcases/'+lis_dir[i+n2],'rb')
+        outp_ = f.read()
+        f.close()
+        
+
+        if p.stdout.replace(b'\r',b'').replace(b'\n',b'') == outp_.replace(b'\r',b'').replace(b'\n',b''):
+            sum += 1
+        else:
+            print("failed for ")
+            print("input: ",inp_.decode())
+            print("expected output: ",outp_.replace(b'\r',b'').replace(b'\n',b'').decode())
+            print("actual output: ",p.stdout.replace(b'\r',b'').replace(b'\n',b'').decode())
+            print("------------------------------------------------")
+
+    
+    print("Test score ",str(sum)+"/"+str(i+1))
+
+try:
+    p=subprocess.run(['pylint',sys.argv[1]],shell=True,check=True,stdout=subprocess.PIPE)
+except Exception as e:
+    print(e.output.replace(b'\r',b'').decode())
+else:
+    print(p.stdout.replace(b'\r\n',b'').decode())
